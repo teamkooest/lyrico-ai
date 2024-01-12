c8c027f8378cc4f602905a3ac43e7a2e3acdb798
Noey Ignacio <hello@noeyislearning.dev>
2024-01-12 13:57:07 +0800

    feat(registration): add logic for admin and client registration

--- server/apps/api/users/urls.py
+++ server/apps/api/users/urls.py
@@ -9,10 +9,15 @@ from .views import (
 )
 
 urlpatterns = [
-  path('list/', UserListView.as_view(), name='users-list'),
-  path('details/<str:pk>/', UserDetailView.as_view(), name='users-detail'),
-  path('register/', UserRegistrationView.as_view(), name='users-register'),
-  path('update/<str:pk>/', UserUpdateView.as_view(), name='users-update'),
-  path('hard-delete/<str:pk>/', UserHardDeleteView.as_view(), name='users-delete'),
-  path('soft-delete/<str:pk>/', UserSoftDeleteView.as_view(), name='users-delete'),
+    # Any (for both admins and clients)
+    path('list/', UserListView.as_view(), name='users-list'),
+    path('details/<str:pk>/', UserDetailView.as_view(), name='users-detail'),
+
+    # Admins
+    path('a/hard-delete/<str:pk>/', UserHardDeleteView.as_view(), name='users-admins-delete'),
+
+    # Clients
+    path('c/register/', UserRegistrationView.as_view(), name='users-clients-register'),
+    path('c/update/<str:pk>/', UserUpdateView.as_view(), name='users-clients-update'),
+    path('c/soft-delete/<str:pk>/', UserSoftDeleteView.as_view(), name='users-clients-delete'),
 ]
