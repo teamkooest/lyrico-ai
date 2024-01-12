b193436f6d9a1d69d1357ee072444fac18a9b7f7
Noey Ignacio <hello@noeyislearning.dev>
2024-01-12 13:58:06 +0800

    feat(admin-dashboard):  separate admins', clients', and all users' table
    Co-authored-by: Ibrahim Lincoln Jr. <jribrahimlincoln@gmail.com>

--- server/apps/api/users/admin.py
+++ server/apps/api/users/admin.py
@@ -1,10 +1,53 @@
+from typing import Any
+from django.core.handlers.wsgi import WSGIRequest
+from django.db import models
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import NewUser
+
+class AdminUserManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset().filter(is_superuser=True, is_staff=True)
+
+class ClientUserManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset().filter(is_superuser=False, is_staff=False)
+
+class AdminUser(NewUser):
+    objects = AdminUserManager()
+
+    class Meta:
+        proxy = True
+        verbose_name = 'Admin'
+        verbose_name_plural = 'Admins'
+
+class ClientUser(NewUser):
+    objects = ClientUserManager()
+    class Meta:
+        proxy = True
+        verbose_name = 'Client'
+        verbose_name_plural = 'Clients'
+
+@admin.register(AdminUser)
+class AdminUserAdmin(UserAdmin):
+    # Customize the display fields for Admin users
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'is_verified', 'is_premium', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_staff', 'is_superuser', 'is_verified', 'is_premium', 'is_active')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
+
+@admin.register(ClientUser)
+class ClientUserAdmin(UserAdmin):
+    # Customize the display fields for Client users
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_verified', 'is_premium', 'is_active', 'created_at', 'updated_at')
+    list_filter = ('is_active',)
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('email',)
 
 @admin.register(NewUser)
 class UserAdmin(admin.ModelAdmin):
-    # Read only fields
-    list_display = ('email', 'username', 'is_staff', 'is_superuser', 'is_verified', 'is_premium', 'is_logged_in', 'is_deleted')
+    # Common configuration for both Admins and Clients
+    list_display = ('email', 'username', 'is_staff', 'is_superuser', 'is_verified', 'is_premium', 'is_logged_in', 'is_deleted', 'is_active', 'created_at', 'updated_at')
     search_fields = ('email', 'username', 'first_name', 'last_name')
     readonly_fields = ('id', 'created_at', 'updated_at')
 
@@ -34,3 +77,4 @@ class UserAdmin(admin.ModelAdmin):
             }
         ),
     )
+
