25466039a7828f7648d7017001133bd5a4e7e73b
Noey Ignacio <hello@noeyislearning.dev>
2024-01-14 00:51:33 +0800

    feat/style(authentication): finish 'Auth' component, remove animation

---
+++ web/src/components/Logos/Logo.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Logo() {
+  return (
+    <Link
+      to="/"
+    >
+      <h1 className="text-4xl font-medium">vocale ai.</h1>
+    </Link>
+  )
+}
